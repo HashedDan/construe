28d4f2d1345c94bbc5a250b01aab96d1180a01ca
Daniel Mangum <daniel.mangum@s01846-mbpr.2d.hq>
2019-01-14 09:27:26 -0600

    first trial with only compute resources defined

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const services = require('./services.json')
+
+const apiUrl = 'https://slack.com/api';
+
+const app = express();
+
+/*
+ * Parse application/x-www-form-urlencoded && application/json
+ * Use body-parser's `verify` callback to export a parsed raw body
+ * that you need to use to verify the signature
+ */
+
+const rawBodyBuffer = (req, res, buf, encoding) => {
+    if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || 'utf8');
+    }
+};
+
+app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
+app.use(bodyParser.json({ verify: rawBodyBuffer }));
+
+/* 
+ * Slash Command
+ * Endpoint to receive /httpstatus slash command from Slack.
+ */
+
+app.post('/command', (req, res) => {
+    let message = {
+    };
+
+    var service = req.body.text.trim();
+    res.send(services[service].aws[0])
+});
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
